b885da380d58290fdbbb1ac53667831975d9629d
jodiemay22 <97054128+jodiemay22@users.noreply.github.com>
2022-07-08 20:51:00 +0100

    Moved code from previous corrupted repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather.js";
+import Forecast from "./Forecast.js";
+import Footer from "./Footer.js";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="Weather-app">
+          <Weather defaultCity="London" />
+          <Forecast />
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
-
-export default App;
